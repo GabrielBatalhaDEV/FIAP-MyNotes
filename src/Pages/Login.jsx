ebe725b79de44a30683b7eb48ac007d0eda1e9d1
Gabriel Batalha <71796398+GabrielBatalhaDEV@users.noreply.github.com>
2022-02-21 20:05:54 -0300

    login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,23 +1,39 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import styles from './login.module.css'
-import Home from '../Pages/Home'
+import { useNavigate } from "react-router-dom";
+import styles from "./login.module.css";
 
 function Login() {
+  const nav = useNavigate();
+
+  function handleUserHome() {
+    console.log("nav");
+    nav("home");
+  }
+
   return (
     <>
-      <div className={Home}>
-        <aside>
-          <div className={styles.foto}></div>
-        </aside>
-        <main className={styles.main}>
-          <form>
-            <input type="text" placeholder="Login" />
-            <input type="password" placeholder="Senha" />
-            <Link to="home">Entrar</Link>
-          </form>
-        </main>
-      </div>
+      <main className={styles.main}>
+        <h1>My Notes</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Login"
+            className={styles.input}
+          ></input>
+          <input
+            type="password"
+            placeholder="Senha"
+            className={styles.input}
+          ></input>
+          <button
+            type="submit"
+            className={styles.button}
+            onClick={handleUserHome}
+          >
+            Entrar
+          </button>
+        </form>
+      </main>
     </>
   );
 }
